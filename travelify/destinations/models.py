a72535d6e7d5d942c298c9c4d93af226957c06fe
aarya-ghimire <aaryaghimire36@gmail.com>
2025-03-25 21:47:21 +0545

    updated models to edit destination specifically

--- travelify/destinations/models.py
+++ travelify/destinations/models.py
@@ -16,6 +16,29 @@ class Destination(models.Model):
     def __str__(self):
         return self.name
 
+    # Optionally add a method to update fields like rating or image if needed
+    def update_fields(self, name=None, description=None, location=None, country=None,
+                      best_time_to_visit=None, activities=None, average_cost=None, rating=None, image=None):
+        if name:
+            self.name = name
+        if description:
+            self.description = description
+        if location:
+            self.location = location
+        if country:
+            self.country = country
+        if best_time_to_visit:
+            self.best_time_to_visit = best_time_to_visit
+        if activities:
+            self.activities = activities
+        if average_cost:
+            self.average_cost = average_cost
+        if rating is not None:
+            self.rating = rating
+        if image:
+            self.image = image
+        self.save()
+
 # Category Model
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
